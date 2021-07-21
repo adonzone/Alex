a026b1320cffb917e39ffa236a471aeae5ef9e64
adonzone <56778024+adonzone@users.noreply.github.com>
2021-07-21 17:03:24 -0400

    Main.py update

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import 
-import time
+import time 
 from time import gmtime, strftime
 
 1 = Input("")
 if 1 = Whats the time :
   Print("It is "strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
+if 1 = How are you :
+  Print("Great")
